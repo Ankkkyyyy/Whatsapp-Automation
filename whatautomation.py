e6f2b58ab33fcd7ccb41d5e15a852485b9392662
Ankit Gireesh Mishra <98558150+Ankkkyyyy@users.noreply.github.com>
2022-03-18 03:46:56 +0530

    Update whatautomation.py

--- whatautomation.py
+++ whatautomation.py
@@ -1,2 +1,6 @@
+#pip install Flask
+#pip install pywhatkit
+
 import pywhatkit as pwt
-pwt.sendwhatmsg("+91 8779099714", "Testing Automation", 3, 33)
+pwt.sendwhatmsg("+91 xxxxxx, "Testing Automation", 3, 33)
+#Enter your Number, Then your message, Time format is 24 hour format not 12 hour format
